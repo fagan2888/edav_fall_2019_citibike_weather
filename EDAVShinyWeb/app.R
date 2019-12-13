e7a0edb90487fac4334f2353203164c56f74a555
VenkataGangireddy <43711846+VenkataGangireddy@users.noreply.github.com>
2019-12-12 23:02:34 -0500

    Edited the scale and filter

--- EDAVShinyWeb/app.R
+++ EDAVShinyWeb/app.R
@@ -1,197 +1,116 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(geojsonio)
+library(tidyverse)
 library(sp)
 library(lubridate)
 library(dplyr)
 library(shiny)
-library( shinythemes )
+library(shinythemes)
 library(eeptools)
+library(ggmap)
+library(sf)
 
-dat <- readRDS(gzcon(url("https://github.com/lecy/CityBikeNYC/raw/master/DATA/bikes.rds")))
-stations <- readRDS(gzcon(url("https://github.com/lecy/CityBikeNYC/raw/master/DATA/STATIONS.rds")))
-routes.list <- readRDS(gzcon(url("https://github.com/lecy/CityBikeNYC/raw/master/DATA/ALL_ROUTES_LIST.rds")))
-water <- geojson_read( "https://raw.githubusercontent.com/lecy/CityBikeNYC/master/DATA/nyc_water.geojson", what="sp" )
+# load data
+citibikedata <- read.csv("https://raw.githubusercontent.com/harish-cu/edav_fall_2019_citibike_weather/master/data/clean/2018-citibike-summarydata.csv")
+#stations <- read.csv("/Users/venkatgangireddy/Documents/GitHub/edav_fall_2019_citibike_weather/data/otherdata/All_NYC_Bike_stations.csv")
+weatherdata <- read_csv('https://raw.githubusercontent.com/harish-cu/edav_fall_2019_citibike_weather/master/data/raw/2018_Weather_NYC.csv')
 
+ggmap_credentials()
+routeQueryCheck()
+API_KEY <- "AIzaSyDHwy7O0EOi3PxtDUI01zOo5PDuRQMoQA0"    # Replace with the Actual google cloud account Key
+register_google( key=API_KEY, account_type="premium", day_limit=10000 )
+ggmap_credentials()
 
-# Create Day of Week Variable in Dat
+routeQueryCheck()
 
-bike.date <- strptime( dat$starttime, format = "%m/%d/%Y" ) 
-day.of.week <- weekdays( bike.date ) # separated data by day
-dat$day.of.week <- day.of.week
+#NYC_Neighborhood <- geojson_read( "/Users/venkatgangireddy/Documents/GitHub/edav_fall_2019_citibike_weather/data/otherdata/NYC_Neighborhood.geojson", what="sp" )
 
-age <- 2019 - dat$birth.year
-rider.age.groups <- c("Younger than 20", "20-29", "30-39", "40-49", "50-59", "60-69", "70+" )
-dat$age <- cut( age, breaks=c(0, 20, 30, 40, 50, 60, 70, 100), labels=rider.age.groups )
-
-table( dat$age )
-
-
-hours <- format(as.POSIXct(dat$starttime, format = "%m/%d/%Y %H:%M"), "%H")
-hours <- as.numeric( as.character( hours ))# transform 
-
-commute.categories <- c("Middle of Night: 12am-5am", "Morning Exercise: 5am-7am", "Morning Commute: 7am-10am", 
-                        "Lunch Ride: 10am-2pm","Afternoon Break: 2pm-4pm","Afternoon Commute: 4pm-7pm", 
-                        "After Dinner Commute: 7pm-10pm","Late Night Commute: 10pm-12am" )
-
-dat$time <- cut( hours, breaks=c(0, 5, 7, 10, 14, 16, 19, 22, 24), labels=commute.categories )
-table( dat$time )
-
-
-
-# COMBINE STEPS INTO A FUNCTION
-#
-# to test:   bike.trips <- dat
-
-plotTrips <- function( bike.trips, max.trip.num, add.water=T, 
-                       line.weight=5, station.size=0.5, background.color="black" )
-{
+#citibikedata$start.station.longitude
+plotRides <- function(nyc.bike.data, weatherdata=weatherdata, station.size=0.5,includeWeather = 'F', max.count=1000, Selecteddate = '2018-08-08' ){
+    #print(Selecteddate)
+    nyc.bike.data <- subset(nyc.bike.data, nyc.bike.data$Date==Selecteddate)
+    #weatherdata <- subset(weatherdata, weatherdata$DATE==Selecteddate)
+    #weatherdata <- weatherdata[1:1,]
+    # print(nrow(nyc.bike.data))
+    # starttrip.count <- as.data.frame( table( nyc.bike.data$start.station.id ) )
+    # This need to be adjusted based on the daily or monthly
     
-    trip.count <- as.data.frame( table( bike.trips$route.id ) )
-    # max.trips <- max( trip.count$Freq )
-    #max.trips <- max( table( bike.trips$route.id, bike.trips$day.of.week ) )
-    trip.weight <- line.weight * ( trip.count$Freq / max.trip.num )
-    
-    max.lat <- 40.77152
-    max.lon <- -73.95005
-    min.lat <- 40.68034
-    min.lon <- -74.01713
-    
-    # dev.new()
-    par( mar=c(0,0,0,0), bg=background.color )
-    plot.new( )
-    plot.window( xlim=c(min.lon,max.lon), ylim=c(min.lat,max.lat) )
-    
-    if( add.water )
-    {
-        # library( geojsonio )
-        # water <- geojson_read( "https://raw.githubusercontent.com/lecy/CityBikeNYC/master/DATA/nyc_water.geojson", what="sp" )
-        
-        plot( water, col="slategrey", border=NA, add=T )
-    }
+    keep.these.vars <- c("start.station.id","start.station.latitude","start.station.longitude","trip_count")
+    stations.data <- unique( nyc.bike.data[ keep.these.vars ] )
+    names( stations.data ) <- c("ID","LAT","LON","COUNT")
+    stations.data <- stations.data[ order( stations.data$ID ) , ]
+    rownames( stations.data ) <- NULL
+    #print(head(stations.data,100))
+    station.size = .5 * nyc.bike.data$trip_count
     
     
-    for( i in 1:nrow( trip.count ) )
-    {
-        single.route <- routes.list[[ trip.count$Var1[i] ]]
-        lines( single.route$lon, single.route$lat, col="gray", lwd=trip.weight[i] )
-        
-    }
+    par( mar=c(0,0,0,0) )
+    #plot( stations.data$LON, stations.data$LAT, col="darkred", pch=19, cex=.5 )
     
-    points( stations$LON, stations$LAT, col="darkorange", pch=19, cex=station.size )
+    nyc <- qmap( 'east village, ny', zoom = 13, color = 'bw' )
+    nyc + geom_point( aes(x = LON, y = LAT, size=COUNT ),alpha = 0.001*stations.data$COUNT, colour="red", data = stations.data )
+    
     
 }
 
+plotRides(nyc.bike.data=citibikedata,weatherdata=weatherdata)
 
-
-plotTrips( bike.trips=dat, max.trip.num=450 )
-
-# Define UI for application 
-ui <- fluidPage(
-
-    
-    theme = shinytheme("cyborg"),
-    #theme = shinytheme("slate"),
-    
-    # Application title
-    titlePanel("Citi Bike NYC Route Traffic"),
-    
-    # Sidebar with a slider input for the number of bins
+ui <- fluidPage (
+    #theme = shinytheme("cyborg"),
+    titlePanel("Weather Impact on NYC Citi Bike Ridership"),
     sidebarLayout(
-        
         sidebarPanel(
-            
             h2( helpText("First Map") ),
             
-            selectInput( inputId="day1", 
-                         label="Select Day of Week", 
-                         choices=c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"),
-                         selected="Monday"
-            ),
-            selectInput( inputId="gender1", 
-                         label="Select Female or Male", 
-                         choices=c("Female"="2","Male"="1"),
-                         selected="1"
-            ), 
-            sliderInput( inputId="time1", 
-                          label="Time of Day", 
-                          min=0, max=24,
-                          value=9 
-                        ),
-            selectInput( inputId="time1", 
-                         label="Time of Day", 
-                         choices = commute.categories,
-                         selected="Morning Exercise: 5am-7am"
-            ),     
-            selectInput( inputId="age1", 
-                         label="Age of Rider", 
-                         choices = rider.age.groups,
-                         selected="30-39" 
+            dateInput('date',
+                      label = 'Date input: yyyy-mm-dd',
+                      value = "2018-08-10"
             ),
             
+            #h2( helpText("Second Map") ),
             
-            h2( helpText("Second Map") ),
+            # sliderInput( inputId="Decile", 
+            #              label="Temperature Range", 
+            #              min=0, max=100,
+            #              value=9 
+            # ),
             
-            selectInput( inputId="day2", 
-                         label="Select Day of Week", 
-                         choices=c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"),
-                         selected="Monday"
-            ),
-            selectInput( inputId="gender2", 
-                         label="Select Female or Male", 
-                         choices=c("Female"="2","Male"="1"),
-                         selected="2"
-            ), 
-            selectInput( inputId="time2", 
-                         label="Time of Day", 
-                         choices= commute.categories,
-                         selected="Morning Exercise: 5am-7am" 
-            ),
-            selectInput( inputId="age2", 
-                         label="Age of Rider", 
-                         choices = rider.age.groups,
-                         selected="30-39"
-            )
+            
         ),
         
-        # Show a plot of the generated distribution
-        mainPanel(  plotOutput( "tripPlot" )  )
-        
+        mainPanel(  
+            #verbatimTextOutput("dateText"),
+            htmlOutput("text"),
+            plotOutput( "tripPlot" )
+        )
     )
 )
 
-# Define server logic 
-server <- function(input, output) {
 
-    output$tripPlot <- renderPlot({  
+server <- function(input,output) {
+    
+    output$text <- renderUI({
         
-        dat.sub1 <- dat[ dat$day.of.week == input$day1 & dat$gender == input$gender1 &
-                             dat$age == input$age1 & dat$time == input$time1 , ]
+        weatherdata$DATE <- mdy(weatherdata$DATE)
+        weatherdata <- filter(weatherdata, DATE==as.character(input$date))
+        str1 <- paste("Date :", as.character(input$date))
+        str2 <- paste("Average Temeprature :", weatherdata$TAVG)
+        str3 <- paste("Average Wind :", weatherdata$AWND)
+        str4 <- paste("Precipitation :", weatherdata$PRCP)
+        str5 <- paste("  ")
         
-        dat.sub2 <- dat[ dat$day.of.week == input$day2 & dat$gender == input$gender2 &
-                             dat$age == input$age2 & dat$time == input$time2 , ]
+        HTML(paste(str1, str2,str3,str4, sep = '<br/>'))
         
-        max.trips <- max( c( table( dat.sub1$route.id ), table( dat.sub2$route.id ) ) )
-        
-        
-        selected.gender1 <- ifelse( input$gender1 == 1, "Male", "Female" )
-        selected.gender2 <- ifelse( input$gender2 == 1, "Male", "Female" )
-        
+    })
+    
+    
+    
+    output$tripPlot <- renderPlot({
+        dat.sub1 <- subset(citibikedata, citibikedata$Date==as.character(input$date))
+        #dat.sub2 <- citibikedata
         par( mfrow=c(1,2) )
-        plotTrips( bike.trips=dat.sub1, max.trip.num=max.trips )
-        title( main=toupper(paste(input$day1,selected.gender1,input$age1,input$time1,sep=" : ")), line=-3, cex.main=1, col.main="white" )
-        plotTrips( bike.trips=dat.sub2, max.trip.num=max.trips )
-        title( main=toupper(paste(input$day2,selected.gender2,input$age2,input$time2,sep=" : ")), line=-3, cex.main=1, col.main="white" )
+        plotRides(nyc.bike.data=dat.sub1,weatherdata=weatherdata, Selecteddate=as.character(input$date))
+        #plotRides(dat.sub1,stations)
         
-    },   height = 800, width = 800 )  # 
+    } )
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(server = server, ui = ui)
